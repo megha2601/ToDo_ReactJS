cc4ef1292761470cf432fe1b35b42cb83948431a
megha2601 <109163494+megha2601@users.noreply.github.com>
2023-06-28 20:08:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import { createRoot } from "react-dom/client";
+import App from "./App";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+//const domNode = document.getElementById("root");
+//const root = createRoot(domNode);
+//root.render(<App />);
+
+ReactDOM.render(<App />, document.getElementById("root"));
